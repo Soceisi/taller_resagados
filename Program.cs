0daed46c2ecfba25c90ea948ace78496ddfa415a
Soceisi <univallesoceisi@gmail.com>
2023-05-13 21:53:48 -0400

    primera version

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace taller_resagados
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+        static void Suma(int n, int n2)
+        {
+            int result = n + n2;
+            Console.WriteLine(result);
+        }
+
+    }
+}
